3d0dc31ec675c371ff49192e4d13e6cb05a1464f
Louise Verbeke <louiseverbeke@yahoo.be>
2026-04-13 15:17:21 -0400

    Initial Commit

---
+++ WorldExplorer/WorldExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldExplorerApp.swift
+//  WorldExplorer
+//
+//  Created by Louise Verbeke on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
